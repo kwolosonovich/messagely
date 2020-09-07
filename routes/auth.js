bc77cb3ba90004555fd5998d6123155bb1cde78d
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-09-07 15:21:20 -0700

    added register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const Router = require("express").Router;
+const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
+const router = new Router();
+
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
@@ -11,3 +19,16 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async function (req, res, next) {
+    try {
+        let { username } = await User.register(req.body);
+        let token = jwt.sign({ username }, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({ token });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
